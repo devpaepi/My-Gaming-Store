0ea0da01950664847185854fd6e36009caefdafc
wonderful samuel <wonder4sure@gmail.com>
2024-04-30 12:20:43 +0100

    day 2 cant even get things together

--- src/components/DownSec.jsx
+++ src/components/DownSec.jsx
@@ -1,7 +1,13 @@
 import "./DownSec.css";
+import Product from "./products/product";
+// import EachProduct from "../each.json";
 
 const DownSec = () => {
-  return <div>DownSec</div>;
+  return (
+    <div>
+      <Product />
+    </div>
+  );
 };
 
 export default DownSec;
---
+++ src/components/products/product.jsx
@@ -0,0 +1,12 @@
+import "./product.css";
+// import EachProduct from "../each.json";
+
+const Product = () => {
+  return (
+    <>
+      <span> This section is coming soon....</span>
+    </>
+  );
+};
+
+export default Product;
